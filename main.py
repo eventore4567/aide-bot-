5e378274a57c10ac50b3046efcc842126d7d400c
eventore4567 <mosbahjayden@gmail.com>
2026-09-12 00:12:47 +0100

    feat: auto-publish interactive center panel

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ log = logging.getLogger("aidebot")
 EXTENSIONS = (
     "aidebot.cogs.training",
     "aidebot.cogs.center",
+    "aidebot.cogs.center_autopost",
     "aidebot.cogs.ticket_recovery",
     "aidebot.cogs.community",
     "aidebot.cogs.diagnostic",
