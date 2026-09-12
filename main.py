cf93b4ebe37c5cd0967ece60aa715e3b147db7b2
eventore4567 <mosbahjayden@gmail.com>
2026-09-12 03:32:45 +0100

    fix: load V50 through acyclic runtime entrypoint

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ EXTENSIONS = (
     "aidebot.cogs.recruitment_panel",
     "aidebot.cogs.recruitment_v46",
     "aidebot.cogs.ticket_experience",
-    "aidebot.cogs.premium_service_v50",
+    "aidebot.cogs.premium_service_v50_runtime",
     "aidebot.cogs.premium_service_v48_runtime",
     "aidebot.cogs.ticket_polish_v44",
     "aidebot.cogs.ticket_recovery",
